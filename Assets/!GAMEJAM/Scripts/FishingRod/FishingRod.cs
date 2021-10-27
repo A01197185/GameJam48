6a29bbcc8c89c1e7b9ee2f2f97b0e6dd83ce7100
LiveDimensions <livedimensions@gmail.com>
2021-10-27 15:00:17 -0500

    Fishing Rod Update

---
+++ Assets/!GAMEJAM/Scripts/FishingRod/FishingRod.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FishingRod : UdonSharpBehaviour
+{
+    [Range(0, 1f)]
+    public float bend = 0f;
+
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/!GAMEJAM/Scripts/FishingRod/RodVerlet.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RodVerlet : UdonSharpBehaviour
+{
+    float maxBendAngle = 20f;
+    [SerializeField] FishingRod fishingRod;
+    
+
+    public void Verlet(float bend)
+    {
+        Vector3 newRotation = new Vector3(maxBendAngle * bend, 0, 0);
+        transform.localRotation = Quaternion.Euler(newRotation);
+    }
+
+    public void Update()
+    {
+        Verlet(fishingRod.bend);
+    }
+}
