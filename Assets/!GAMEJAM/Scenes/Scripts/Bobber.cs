44880de9a3e9764220bbc65bc8417dd437cfe719
shukishan <shukishan@gmail.com>
2021-10-27 09:54:26 -0500

    bobber

---
+++ Assets/!GAMEJAM/Scenes/Scripts/Bobber.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Bobber : UdonSharpBehaviour {
+
+    public float underDrag = 1.5f, underAngularDrag = 2f, airDrag = 0f, airAngularDrag = 0.05f, floatingPower = 70f, waterHeight = 0f;
+    public bool catching = false;
+
+    private Rigidbody bobber;
+    private bool up = true;
+
+    void Start() {
+        bobber = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        if (catching && up && floatingPower > 30f) {
+            floatingPower -= 1f;
+        } else if(catching) {
+            floatingPower += 1f;
+            up = floatingPower > 90f;
+        } else {
+            floatingPower = 70f;
+        }
+        if (((transform.position.y - waterHeight) < 0)) {
+            bobber.AddForceAtPosition(Vector3.up*floatingPower*Mathf.Abs((transform.position.y - waterHeight)), transform.position, ForceMode.Force);
+            bobber.drag = underDrag;
+            bobber.angularDrag = underAngularDrag;
+        } else {
+            bobber.drag = airDrag;
+            bobber.angularDrag = airAngularDrag;
+        }
+    }
+}
